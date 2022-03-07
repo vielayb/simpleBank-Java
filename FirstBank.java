68a6f518b3a67c785ea4f03f16ee52e2b52ef4b6
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-03-07 11:00:15 -0500

    first commit

---
+++ FirstBank.java
@@ -0,0 +1,11 @@
+/**
+ * FirstBank
+ */
+public class FirstBank {
+
+    public static void main(String[] args) {
+        
+        Account customer = new Account("New Customer", "001");
+        customer.showMenu();
+    }
+}
